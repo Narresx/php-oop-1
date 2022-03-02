5ee8476e6ee3c727aebd090a1e73d85650d22395
Narresx <92748159+Narresx@users.noreply.github.com>
2022-03-02 16:01:43 +0100

    add echos rates

--- index.php
+++ index.php
@@ -29,24 +29,31 @@ public function getThought(){
 
 public function goodMovie(){
     if($this->rate > 7){
-        $this->thought = 'Is a good film';
+        $this->thought = ' Is a good film';
     } else {
-        $this->thought = 'Is not a good film';
+        $this->thought = ' Is not a good film';
     }
 }
 
 }
 
-$movie1 = new Movie('I am legend', 'adventure', 10);
-$movie2 = new Movie('Batman Begins', 'adventure', 6);
+$movie1 = new Movie('I am legend.', ' adventure ', 10);
+$movie2 = new Movie('Batman Begins.', ' adventure ', 6);
 
 $movie1->goodMovie();
+echo $movie1->getTitle();
+echo $movie1->getGenres();
+echo $movie1->getRates();
 echo $movie1->getThought();
 
-var_dump($movie1);
+/* var_dump($movie1); */
 
 $movie2->goodMovie();
+echo $movie2->getTitle();
+echo $movie2->getGenres();
+echo $movie2->getRates();
 echo $movie2->getThought();
-var_dump($movie2);
+
+/* var_dump($movie2); */
 
 ?>
